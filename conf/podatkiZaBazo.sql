a571887425d356f59d80a6f205af90bdeee4ce49
andrazpovse <andraz.povse1@gmail.com>
2017-11-30 15:14:51 +0100

    Dodan opis izdelka v tabelo izdelek. Spremenjeno v ER in v stavkih za kreiranje zapisov

--- conf/podatkiZaBazo.sql
+++ conf/podatkiZaBazo.sql
@@ -2,6 +2,6 @@ INSERT INTO uporabnik (vloga, ime, priimek, email, geslo) VALUES ('administrator
 INSERT INTO uporabnik (vloga, ime, priimek, email, geslo, naslov, telefon) VALUES ('stranka', 'Johnny', 'Bravo', 'jbravo@mail.com', 'heymomma', 'večšžna 123, lj', '0123456789');
 
 
-INSERT INTO izdelek(ime, cena) VALUES ('Mountain Dew', '0.60');
-INSERT INTO izdelek(ime, cena) VALUES ('Capita DOA Snowboard', '439.90');
-INSERT INTO izdelek(ime, cena) VALUES ('USB key - 128GB', '22.99');
+INSERT INTO izdelek(ime, cena, opis) VALUES ('Mountain Dew', '0.60', 'Get your MLG on');
+INSERT INTO izdelek(ime, cena, opis) VALUES ('Capita DOA Snowboard', '439.90', 'Take on the local hill with this beast');
+INSERT INTO izdelek(ime, cena, opis) VALUES ('USB key - 128GB', '22.99', 'Expand your storage with this.');
