a24e28049ae5913cb4cb9f1bee3a2411d7873be9
andrazpovse <andraz.povse1@gmail.com>
2017-11-30 15:46:41 +0100

    V podatkeZaBazo dodan dummy data za oceno in sliko, tako da index.php pravilno prikaze var dump

--- conf/podatkiZaBazo.sql
+++ conf/podatkiZaBazo.sql
@@ -5,3 +5,6 @@ INSERT INTO uporabnik (vloga, ime, priimek, email, geslo, naslov, telefon) VALUE
 INSERT INTO izdelek(ime, cena, opis) VALUES ('Mountain Dew', '0.60', 'Get your MLG on');
 INSERT INTO izdelek(ime, cena, opis) VALUES ('Capita DOA Snowboard', '439.90', 'Take on the local hill with this beast');
 INSERT INTO izdelek(ime, cena, opis) VALUES ('USB key - 128GB', '22.99', 'Expand your storage with this.');
+
+INSERT INTO ocena(uporabnik_id, izdelek_id, ocena) VALUES (1,2,5);
+INSERT INTO slika(path, izdelek_id) VALUES ('PotDoSlike.png', 2);
