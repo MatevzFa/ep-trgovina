61008a89bd0b0432c2ec54091a19c2b15893a03f
andrazpovse <andraz.povse1@gmail.com>
2017-12-04 16:46:51 +0100

    Bootstraped izdelki detail (ceprav zgleda dejansko isto :D )

--- php/view/izdelki-detail.php
+++ php/view/izdelki-detail.php
@@ -1,23 +1,39 @@
 <!DOCTYPE html>
 
-<link rel="stylesheet" type="text/css" href="<?= CSS_URL . "style.css" ?>">
-<meta charset="UTF-8" />
-<title>Trgovina</title>
-
-<?php
-    //var_dump($izdelek);
-    //var_dump($slike);
-    if ($izdelek['povprecnaOcena'] == null) {
-    	$izdelek['povprecnaOcena'] = 'Ta izdelek se nima ocene.';
-    }
-?>
-
-<h1><?= $izdelek['ime'] ?></h1>
-
-<?php foreach ($slike as $slika): ?>
-        <p> Slika: <?= $slika['path'] ?></p>
-<?php endforeach; ?>
-
-<p><?= $izdelek['cena'] ?></p>
-<p><?= $izdelek['opis'] ?></p>
-<p>Povprecna ocena: <?= $izdelek['povprecnaOcena'] ?></p>
+<head>
+	<title>Trgovina</title>
+	<link rel="stylesheet" type="text/css" href="<?= CSS_URL . "style.css" ?>">
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<ul class="nav nav-pills">
+					<li class="active">
+						 <a href="<?= BASE_URL . "izdelki" ?>""> <span class="badge pull-right"></span> Nazaj</a>
+					</li>
+				</ul>
+				<div class="row">
+					<div class="col-md-8">
+						<h1><?= $izdelek['ime'] ?></h1>
+						<?php foreach ($slike as $slika): ?>
+					        <p> Slika: <?= $slika['path'] ?></p>
+						<?php endforeach; ?>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-8">
+						<p><?= $izdelek['cena'] ?> £</p>
+						<p><?= $izdelek['opis'] ?></p>
+						<p>Povprecna ocena: <?= $izdelek['povprecnaOcena'] ?></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
