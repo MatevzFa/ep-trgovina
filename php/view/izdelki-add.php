372188144643c01edbc6216729f2a24daa9eba74
Matevž Fabjančič <mf6422@student.uni-lj.si>
2017-12-08 12:29:07 +0100

    Dodan navbar na izdelki-add in izdelki-detail

--- php/view/izdelki-add.php
+++ php/view/izdelki-add.php
@@ -1,40 +1,36 @@
 <!DOCTYPE html>
+<html>
 
-<head>
-    <title>Dodaj nov izdelek</title>
-    <link rel="stylesheet" type="text/css" href="<?= CSS_URL . "style.css" ?>">
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?= BOOTSTRAP . "css/bootstrap.min.css" ?>">
-    <script src="<?= BOOTSTRAP . "js/bootstrap.min.js" ?>"></script>
-</head>
+    <head>
+        <title>Dodaj nov izdelek</title>
+        <?php include(HEAD); ?>
+    </head>
 
+    <body>
+        <?php include(NAVBAR); ?>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="nav nav-pills">
+                        <li class="active">
+                            <a href="<?= BASE_URL . "izdelki" ?>""> <span class="badge pull-right"></span> Nazaj</a>
+                        </li>
+                    </ul>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <br>
+                            <form action="<?= BASE_URL . "izdelki/add" ?>" method="post">
+                                <p><label>Ime: <input type="text" name="ime" value="<?= $ime ?>" autofocus /></label></p>
+                                <p><label>Cena: <input type="number" step="0.01" min="0" name="cena" value="<?= $cena ?>" /></label></p>
+                                <p><label>Opis: <input type="text" name="opis" value="<?= $opis ?>" /></label></p>
+                                <p><button>Insert</button></p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
 
-
-<body>
-<div class="container-fluid">
-		<div class="row">
-			<div class="col-md-12">
-				<ul class="nav nav-pills">
-					<li class="active">
-						 <a href="<?= BASE_URL . "izdelki" ?>""> <span class="badge pull-right"></span> Nazaj</a>
-					</li>
-				</ul>
-				<div class="row">
-					<div class="col-md-8">
-                                            <br>
-						<form action="<?= BASE_URL . "izdelki/add" ?>" method="post">
-                                                    <p><label>Ime: <input type="text" name="ime" value="<?= $ime ?>" autofocus /></label></p>
-                                                    <p><label>Cena: <input type="number" step="0.01" min="0" name="cena" value="<?= $cena ?>" /></label></p>
-                                                    <p><label>Opis: <input type="text" name="opis" value="<?= $opis ?>" /></label></p>
-                                                    <p><button>Insert</button></p>
-                                                </form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</body>
-    
 </body>
 </html>
--- php/view/izdelki-detail.php
+++ php/view/izdelki-detail.php
@@ -1,38 +1,40 @@
 <!DOCTYPE html>
 
-<head>
-    <title>Trgovina</title>
-    <link rel="stylesheet" type="text/css" href="<?= CSS_URL . "style.css" ?>">
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?= BOOTSTRAP . "css/bootstrap.min.css" ?>">
-    <script src="<?= BOOTSTRAP . "js/bootstrap.min.js" ?>"></script>
-</head>
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <ul class="nav nav-pills">
-                    <li class="active">
-                        <a href="<?= BASE_URL . "izdelki" ?>""> <span class="badge pull-right"></span> Nazaj</a>
-                    </li>
-                </ul>
-                <div class="row">
-                    <div class="col-md-8">
-                        <h1><?= $izdelek['ime'] ?></h1>
-                        <?php foreach ($slike as $slika): ?>
-                            <p> Slika: <?= $slika['path'] ?></p>
-                        <?php endforeach; ?>
+<html>
+
+    <head>
+        <title>Trgovina</title>
+        <?php include(HEAD); ?>
+    </head>
+
+    <body>
+        <?php include(NAVBAR); ?>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="nav nav-pills">
+                        <li class="active">
+                            <a href="<?= BASE_URL . "izdelki" ?>""> <span class="badge pull-right"></span> Nazaj</a>
+                        </li>
+                    </ul>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h1><?= $izdelek['ime'] ?></h1>
+                            <?php foreach ($slike as $slika): ?>
+                                <p> Slika: <?= $slika['path'] ?></p>
+                            <?php endforeach; ?>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-8">
-                        <p><?= $izdelek['cena'] ?> £</p>
-                        <p><?= $izdelek['opis'] ?></p>
-                        <p>Povprecna ocena: <?= isset($izdelek['povprecnaOcena']) ? $izdelek['povprecnaOcena'] : "Ni ocen" ?></p>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p><?= $izdelek['cena'] ?> £</p>
+                            <p><?= $izdelek['opis'] ?></p>
+                            <p>Povprecna ocena: <?= isset($izdelek['povprecnaOcena']) ? $izdelek['povprecnaOcena'] : "Ni ocen" ?></p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
+
+</html>
