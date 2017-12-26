dc10a92d05611bcb84c6a03fdcd83f88efcd710f
Matevž Fabjančič <mf6422@student.uni-lj.si>
2017-12-26 13:14:16 +0100

    Hashirana gesla v data.sql, seznam uporabnikov z gesli

--- conf/data.sql
+++ conf/data.sql
@@ -9,8 +9,8 @@ TRUNCATE slika;
 TRUNCATE narocilo;
 TRUNCATE narocilo_vsebuje;
 
-INSERT INTO uporabnik (vloga, ime, priimek, email, geslo) VALUES ('administrator', 'Janez', 'Novak', 'jno@mail.com', 'geslo123');
-INSERT INTO uporabnik (vloga, ime, priimek, email, geslo, naslov, telefon) VALUES ('stranka', 'Johnny', 'Bravo', 'jbravo@mail.com', 'heymomma', 'večšžna 123, lj', '0123456789');
+INSERT INTO uporabnik (vloga, ime, priimek, email, geslo) VALUES ('administrator', 'Janez', 'Novak', 'jno@mail.com', '$2y$10$F5FYbd8DNyGts4oKoMnNcuSuPjdSMjB0ZbyUz9dtFtQmXsj/uS482'); -- geslo123
+INSERT INTO uporabnik (vloga, ime, priimek, email, geslo, naslov, telefon) VALUES ('stranka', 'Johnny', 'Bravo', 'jbravo@mail.com', '$2y$10$kTqDDNXfTkg.K98WLyFu9.jrBMze1FZ7ieAk6lUS5H7nNcI4/t2ei', 'večšžna 123, lj', '0123456789'); -- heymomma
 
 INSERT INTO izdelek(ime, cena, opis) VALUES ('Mountain Dew', '0.60', 'Get your MLG on');
 INSERT INTO izdelek(ime, cena, opis) VALUES ('Capita DOA Snowboard', '439.90', 'Take on the local hill with this beast');
@@ -19,8 +19,8 @@ INSERT INTO izdelek(ime, cena, opis) VALUES ('USB key - 128GB', '22.99', 'Expand
 INSERT INTO ocena(uporabnik_id, izdelek_id, ocena) VALUES (1,2,5);
 INSERT INTO slika(path, izdelek_id) VALUES ('PotDoSlike.png', 2);
 
-INSERT INTO uporabnik (vloga, ime, priimek, email, geslo) VALUES ('prodajalec', 'James', 'Berry', 'james@berry.com', 'abcdefg');
-INSERT INTO uporabnik (vloga, ime, priimek, email, geslo, naslov, telefon) VALUES ('stranka', 'Ivan', 'Smith', 'ivan@gmail.com', 'ivanivanpass', 'Trubarjeva cesta 5, 1000 Ljubljana', '031245199');
+INSERT INTO uporabnik (vloga, ime, priimek, email, geslo) VALUES ('prodajalec', 'James', 'Berry', 'james@berry.com', '$2y$10$Tz1PiIfB8WML0VxAsdpDzect3MZ7s9V865mQnmmAIqc6LZU4RpCaK'); -- abcdefg
+INSERT INTO uporabnik (vloga, ime, priimek, email, geslo, naslov, telefon) VALUES ('stranka', 'Ivan', 'Smith', 'ivan@gmail.com', '$2y$10$zc4c3w8WnBsgwFBySYNhluSY7kT9k8CuEf92yXNitzHwIqMVxTeuK', 'Trubarjeva cesta 5, 1000 Ljubljana', '03124519'); -- ivanivanpass
 INSERT INTO ocena(uporabnik_id, izdelek_id, ocena) VALUES (2,2,4);
 INSERT INTO izdelek(ime, cena, opis) VALUES ('Xiaomi Mi Robot Vacuum', '274.59', 'Forget about vacuum cleaning. Get a robot.');
 
---
+++ conf/uporabniki.md
@@ -0,0 +1,16 @@
+
+## Janez Novak, administrator  
+email: `jno@mail.com`  
+geslo: `geslo123`
+
+## Johnny Bravo, stranka  
+email: `jbravo@mail.com`  
+geslo: `heymomma`
+
+## James Berry, prodajalec  
+email: `james@berry.com`  
+geslo: `abcdefg`
+
+## Ivan Smith, stranka  
+email: `ivan@gmail.com`  
+geslo: `ivanivanpass`
