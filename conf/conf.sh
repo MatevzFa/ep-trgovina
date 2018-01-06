ceef103870c610a10e59a741de346c2a2a977aa8
Matevž Fabjančič <mf6422@student.uni-lj.si>
2018-01-06 18:01:09 +0100

    Skripta za inicializacijo strežnika

---
+++ conf/conf.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+conf_apache2="apache2"
+conf_certs="certifikati"
+conf_db="mysql"
+
+APACHE="/etc/apache2"
+
+# Apache2 mods
+function a2enmods() {
+    echo "Konfiguracija Apache2 modulov..."
+	sudo a2enmod rewrite ssl
+    echo
+}
+
+# Apache2 sites-available
+function a2conf() {
+    echo "Konfiguracija strežnika Apache2..."
+
+	for conffile in $conf_apache2/*conf; do
+
+		diff $conffile $APACHE/sites-available/${conffile##*/} >/dev/null
+		isdiff=$?
+
+		if (($isdiff)); then
+			sudo mv "$APACHE/sites-available/${conffile##*/}" "$APACHE/sites-available/${conffile##*/}.$(date +%Y%m%d_%H%M%S).bak"
+			sudo cp "$conffile" "$APACHE/sites-available/"
+			echo "Datoteka $conffile kopirana v $APACHE/sites-available/"
+		else
+            echo "V $conffile ni sprememb."
+        fi
+	done
+    echo
+}
+
+# Server certificates, CA certificates, CRL
+function certs() {
+    echo "Konfiguracija strežniških certifikatov..."
+	sudo cp $conf_certs/*.pem $APACHE/ssl/
+    echo
+}
+
+function initdb() {
+    echo "Inicializacija podatkovne baze..."
+	cd $conf_db
+	cat baza.sql data.sql | mysql -uroot -p
+	cd ..
+    echo
+}
+
+if (($#)); then
+	for arg in $@; do
+		$arg
+	done
+else
+    a2enmods
+    a2conf
+    certs
+    initdb
+fi
