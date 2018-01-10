8320eee7003b90218e8991a2c4a0433eb3f613ab
Matevž Fabjančič <mf6422@student.uni-lj.si>
2018-01-10 23:28:07 +0100

    Cleanup kosarica.component

--- php/view/components/kosarica.component.php
+++ php/view/components/kosarica.component.php
@@ -1,17 +1,9 @@
 <table class="table table-hover">
     <?php foreach ($kosarica as $izdelek): ?>
-        <!-- <div class="border rounded row">
-            <div class="col-6"><a href="<?= BASE_URL . 'izdelki?id=' . $izdelek['id'] ?>"><?= $izdelek['ime'] ?></a></div>
-            <div class="col-2"><?= $izdelek['kolicina'] ?></div>
-            <div class="col-4"><?= $izdelek['kolicina'] * $izdelek['cena'] ?></div>
-        </div> -->
-
         <tr>
             <td class=""><a href="<?= BASE_URL . 'izdelki?id=' . $izdelek['id'] ?>"><?= $izdelek['ime'] ?></a></td>
             <td class=""><?= $izdelek['kolicina'] ?></td>
             <td class=""><?= $izdelek['kolicina'] * $izdelek['cena'] ?></td>
         </tr>
-
-
     <?php endforeach; ?>
 </table>
